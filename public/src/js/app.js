80ea5d92e24de02a187c5ee76733a3cf91803032
Sami Ullah <sami.ullah@cadt-solutions.com>
2018-09-26 17:37:27 +0400

    Added code for handling home screen install banner

--- public/src/js/app.js
+++ public/src/js/app.js
@@ -1,3 +1,6 @@
+let deferredPrompt;
+const installBtn = document.querySelector('button');
+
 if('serviceWorker' in navigator){
   navigator.serviceWorker
     .register('/sw.js')
@@ -5,3 +8,28 @@ if('serviceWorker' in navigator){
       console.log('Service Worker Registered!')
     })
 }
+
+window.addEventListener("beforeinstallprompt", (event) => {
+  console.log('beforeinstallprompt fired');
+  event.preventDefault();
+  deferredPrompt = event;
+  return false;
+})
+
+installBtn.addEventListener('click', () => {
+  if(deferredPrompt){
+    deferredPrompt.prompt();
+
+    deferredPrompt.userChoice.then((result)=>{
+      console.log(result.outcome);
+      if(result.outcome === 'dismissed'){
+        console.log('User cancelled installation');
+      } else {
+        console.log('Use added to Home Screen');
+        
+      }
+
+      deferredPrompt = null;
+    })
+  }
+})
